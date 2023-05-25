4eac1c604dcae2b036f29b179f9d0ca7344390ed
hheadake <113627073+hheadake@users.noreply.github.com>
2023-05-25 13:18:16 +0300

    selecting the radio btn

--- 04.Locked-Profile/app.js
+++ 04.Locked-Profile/app.js
@@ -1,3 +1,30 @@
 function lockedProfile() {
-    console.log('TODO...')
+   
+
+let unlockBtn = document.getElementById('unlocked');
+let showMoreBtn = document.querySelector('button');
+
+showMoreBtn.addEventListener('click', () => {
+if (unlockBtn.checked == true) {
+
+   
+
+}
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
