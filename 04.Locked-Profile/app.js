ab3630163a74251177c098194bc909c5e4d41e9f
hheadake <113627073+hheadake@users.noreply.github.com>
2023-05-25 13:29:29 +0300

    making request

--- 04.Locked-Profile/app.js
+++ 04.Locked-Profile/app.js
@@ -4,10 +4,27 @@ function lockedProfile() {
 let unlockBtn = document.getElementById('unlocked');
 let showMoreBtn = document.querySelector('button');
 
+async function makingRequest(url) {
+
+    let response = await fetch(url);
+    let data = await response.json()
+    console.log(Object.values(data))
+
+
+
+}
+
+
+
+
+
+
 showMoreBtn.addEventListener('click', () => {
 if (unlockBtn.checked == true) {
 
-   
+makingRequest('http://localhost:3030/jsonstore/advanced/profiles')
+
+
 
 }
 
