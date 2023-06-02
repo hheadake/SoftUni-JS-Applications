633e125fc7099ada6ae75dd05a5da6c277f57649
hheadake <113627073+hheadake@users.noreply.github.com>
2023-06-02 22:06:50 +0300

    Accordion task
    Getting data from server

--- 05. Accordion/accordion.js
+++ 05. Accordion/accordion.js
@@ -1,3 +1,36 @@
-function solution() {
-    //TODO .....
-}
+const mainEl = document.getElementById('main');
+import { createProfile } from "../04.Locked-Profile/creatingProfile.js";
+
+async function solution(){
+ 
+const url = 'http://localhost:3030/jsonstore/advanced/articles/list'
+
+const response = await fetch(url);
+const data = await response.json();
+
+data.forEach(element => {
+
+    const divElement = createProfile('div',false, false, mainEl,'accordion')
+    const innerDiv = createProfile('div', false, false, divElement,'head')
+    createProfile('span', false, `${element.title}`, innerDiv, false)
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} solution();
