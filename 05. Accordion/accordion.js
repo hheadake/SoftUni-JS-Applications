aab250405ad23519474e820e8acc6777d18923fc
hheadake <113627073+hheadake@users.noreply.github.com>
2023-06-02 23:02:37 +0300

    btn changes
    setting the right id to the btns

--- 05. Accordion/accordion.js
+++ 05. Accordion/accordion.js
@@ -1,6 +1,7 @@
 const mainEl = document.getElementById('main');
 import { createProfile } from "../04.Locked-Profile/creatingProfile.js";
 
+
 async function solution(){
  
 const url = 'http://localhost:3030/jsonstore/advanced/articles/list'
@@ -10,10 +11,10 @@ const data = await response.json();
 
 data.forEach(element => {
 
-    const divElement = createProfile('div',false, false, mainEl,'accordion')
-    const innerDiv = createProfile('div', false, false, divElement,'head')
-    createProfile('span', false, `${element.title}`, innerDiv, false)
-
+    const divElement = createProfile('div',false, false, mainEl,'accordion');
+    const innerDiv = createProfile('div', false, false, divElement,'head');
+    createProfile('span', false, `${element.title}`, innerDiv, false);
+    const btn = createProfile('button', `${element._id}`, 'More', innerDiv, 'button');
 
 
 
