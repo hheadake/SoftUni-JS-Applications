4773350c002637e239e3c8dc347b518c41005f90
hheadake <113627073+hheadake@users.noreply.github.com>
2023-06-03 15:07:43 +0300

    Update app.js

--- 06.Blog/app.js
+++ 06.Blog/app.js
@@ -2,38 +2,44 @@ function attachEvents() {
     
 const loadBtn = document.getElementById('btnLoadPosts');
 const optionField = document.getElementById('posts');
+const postBtn = document.getElementById('btnViewPost')
+
 loadBtn.addEventListener('click', async function getData() { 
 
 const url = 'http://localhost:3030/jsonstore/blog/posts'
 
 const request = await fetch(url);
 const response = await request.json();
-const data = Object.keys(response)
-
-
 
  for (const key in response) {
     
         const element = response[key];
         const option = createEl('option', `${element.id}`, `${element.title}`, optionField, false)
-
-    
-
-    
+   
  }
     
 
 
 });
 
+    postBtn.addEventListener('click', getComments)
+
+    async function getComments(e) {
+
+        const url = 'http://localhost:3030/jsonstore/blog/comments'
+
+        const request = await fetch(url);
+        const response = await request.json();
+        for (const key in response) {
+    
+            const element = response[key];
+           console.log(element.text)
+       
+     }
+        
 
 
-
-
-
-
-
-
+    }
 
 
 
@@ -41,6 +47,25 @@ const data = Object.keys(response)
 }
 
 attachEvents();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function createEl(element,value, text, appender, nameOfClass) {
 
