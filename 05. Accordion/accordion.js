bcdb8a5281bbc8a8c9850b076ba63dc5893cdcb4
hheadake <113627073+hheadake@users.noreply.github.com>
2023-06-03 10:10:03 +0300

    update
    Adding try catch

--- 05. Accordion/accordion.js
+++ 05. Accordion/accordion.js
@@ -6,32 +6,40 @@ async function solution(){
  
 const url = 'http://localhost:3030/jsonstore/advanced/articles/list'
 
-const response = await fetch(url);
-const data = await response.json();
-
-data.forEach(element => {
-
-    const divElement = createProfile('div',false, false, mainEl,'accordion');
-    const innerDiv = createProfile('div', false, false, divElement,'head');
-    createProfile('span', false, `${element.title}`, innerDiv, false);
-    const btn = createProfile('button', `${element._id}`, 'More', innerDiv, 'button');
-
-
-
-});
-
-
-
-
-
-
-
-
-
-
-
+try {
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    
+    data.forEach(element => {
+        
+        const divElement = createProfile('div',false, false, mainEl,'accordion');
+        const innerDiv = createProfile('div', false, false, divElement,'head');
+        createProfile('span', false, `${element.title}`, innerDiv, false);
+        const btn = createProfile('button', `${element._id}`, 'More', innerDiv, 'button');
+        const extraDiv = createProfile('div', false, false, divElement);
+        btn.addEventListener('click', setData);
+        async function setData (e) {
+            let id = e.target.id
+            const url = `http://localhost:3030/jsonstore/advanced/articles/details/${id}`
+            
+            const req = await fetch(url);
+            const res = await req.json();
+            
+            
+            let pEl = createProfile('p', false,`${res.content}`, extraDiv, false)
+            
+        }
+        
+        
+    });
+    
+}  catch (error) {
+    console.log(err)
+ }
 
 
 
 
 } solution();
+
